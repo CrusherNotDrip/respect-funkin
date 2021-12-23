3de4688e99287fb874517937e9682ebdbf57e49d
CrusherMods <90648119+CrusherMods@users.noreply.github.com>
2021-12-22 21:28:18 -0500

    sorry whoops

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '0.4.2'; //This is also used for Discord RPC
-    public static var respectVersion:String = '9.6';
+    public static var respectVersion:String = '9.5';
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
