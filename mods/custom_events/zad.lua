6799974a947104005bed57b18aded4870795ec68
CrusherMods <90648119+CrusherMods@users.noreply.github.com>
2021-12-25 23:55:56 -0500

    NEW UPDATE WOOHOO (you'll get the outdated warning for a reason now)

--- mods/custom_events/zad.lua
+++ mods/custom_events/zad.lua
@@ -2,7 +2,6 @@ function onEvent(name, value1, value2)
 	if name == "zad" then
 		makeLuaSprite('image', 'sad', 0, 0);
 		addLuaSprite('image', true);
-		playSound('BOOM');
 		doTweenColor('hello', 'image', 'FFFFFFFF', 0.2, 'quartIn');
 		setObjectCamera('image', 'other');
 		runTimer('wait', 0.6);
--- source/PunchingRoxState.hx
+++ source/PunchingRoxState.hx
@@ -1,6 +1,9 @@
 // Would this be used from the main menu or what? if its used ingame then dont put state (Just letting you know)
 package;
 
+#if desktop
+import Discord.DiscordClient;
+#end
 import flixel.FlxState;
 import flixel.FlxSprite;
 
@@ -8,6 +11,11 @@ class PunchingRoxState extends MusicBeatState
 {
     override public function create()
     {
+        #if desktop
+		// Updating Discord Rich Presence
+		DiscordClient.changePresence("Beating Up Rox", null);
+		#end
+
 		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
 		bg.setGraphicSize(Std.int(bg.width * 1.175));
 		bg.updateHitbox();
